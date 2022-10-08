d4d0760fc8e1d7bff3e9d044a74c9c6afecac628
iankesh <iankesh@hotmail.com>
2022-10-08 22:26:17 +0530

    adding mockito changes

--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -1,15 +1,20 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.configuration.IMockitoConfiguration;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalTime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class RestaurantTest {
     Restaurant restaurant;
     //REFACTOR ALL THE REPEATED LINES OF CODE
+    @BeforeEach
     private void addRestaurantDetails() {
         LocalTime openingTime = LocalTime.parse("09:00:00");
         LocalTime closingTime = LocalTime.parse("20:00:00");
@@ -23,18 +28,19 @@ class RestaurantTest {
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
-//        restaurant  = new Restaurant("Restaurant X", "Bengaluru", LocalTime.of(7, 0),LocalTime.of(23, 30));
-        addRestaurantDetails();
-        assertTrue(restaurant.isRestaurantOpen());
+        Restaurant restaurant1 = Mockito.spy(restaurant);
+        LocalTime nowTime = LocalTime.parse("11:30:00");
+        Mockito.when(restaurant1.getCurrentTime()).thenReturn(nowTime);
+        assertTrue(restaurant1.isRestaurantOpen());
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
-        restaurant = new Restaurant("Restaurant Y", "Mumbai", LocalTime.of(13, 30),LocalTime.of(14, 30));
-//        assertEquals(false, testR.isRestaurantOpen());
-        assertFalse(restaurant.isRestaurantOpen());
-
+        Restaurant restaurant1 = Mockito.spy(restaurant);
+        LocalTime nowTime = LocalTime.parse("23:55:00");
+        Mockito.when(restaurant1.getCurrentTime()).thenReturn(nowTime);
+        assertFalse(restaurant1.isRestaurantOpen());
     }
 
     //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -43,43 +49,24 @@ class RestaurantTest {
     //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void adding_item_to_menu_should_increase_menu_size_by_1(){
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119 );
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.addToMenu("Sizzling brownie",319);
         assertEquals(initialMenuSize+1,restaurant.getMenu().size());
     }
     @Test
     public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         int initialMenuSize = restaurant.getMenu().size();
         restaurant.removeFromMenu("Vegetable lasagne");
         assertEquals(initialMenuSize-1,restaurant.getMenu().size());
     }
     @Test
     public void removing_item_that_does_not_exist_should_throw_exception() {
-        LocalTime openingTime = LocalTime.parse("10:30:00");
-        LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
     }
 
     @Test
     public void adding_items_to_menu_should_return_non_zero_total_price() {
-        addRestaurantDetails();
         restaurant.addToMenu("Chicken Tikka", 212);
         assertEquals(600, restaurant.getTotalCost());
     }
